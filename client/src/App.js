cd01237f6006ba7bafa7ff9233edf4cdf4a67cb6
kitdd1119 <https://github.com/kitdd1119/my_meat_project.git>
2024-02-14 23:11:20 +0900

    react 학습 6

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage"
+import LoginPage from "./components/views/LoginPage/LoginPage"
+import RegisterPage from "./components/views/RegisterPage/RegisterPage"
 
+/* 경로 설정해 줄 때 element 말고 components 사용해도 될 듯 */
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <Router> 
+      <div>
+        <Routes> 
+          <Route exact path="/" element={<LandingPage />} /> 
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} /> 
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
